bde38fd01c2cfea6a1fb154d796d9943f82d116a
abhishekkumar <kumar.abhishe@northeastern.edu>
2024-02-12 22:34:18 -0500

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Profile from './components/profile'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Profile /> 
     </div>
   )
 }
